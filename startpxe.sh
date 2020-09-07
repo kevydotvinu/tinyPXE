117a88af3e3fa4d7f5bf63367523cdbb7a0587aa
Vinu K <kv_vinu@hotmail.com>
2020-09-07 11:38:39 +0530

    firewall-cmd ports

--- startpxe.sh
+++ startpxe.sh
@@ -7,5 +7,10 @@ mkfs -t xfs -n ftype=1 /dev/vgDATA/lvRUN
 mount /dev/vgDATA/lvLIB /var/lib/containers
 mount /dev/vgDATA/lvRUN /var/run/containers
 
+firewall-cmd --add-port=4011/udp --permanent
+firewall-cmd --add-port=67/udp --permanent
+firewall-cmd --add-port=69/udp --permanent
+firewall-cmd --reload
+
 buildah bud --security-opt label=disable --tag localhost/kevydotvinu/pxe:v1 .
 podman run --rm -it --privileged --net host -v "$(pwd)/tftpboot:/var/lib/tftpboot" -v "$(pwd)/dnsmasq.conf.dhcpproxy:/etc/dnsmasq.conf" --security-opt label=disable --name=pxe localhost/kevydotvinu/pxe:v1 --interface=vboxnet0
