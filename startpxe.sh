310f9f8f192995ac6a7eaf13f74d85d1d353d8a5
Vinu K <kv_vinu@hotmail.com>
2020-09-08 10:42:01 +0530

    edited startpxe.sh

--- startpxe.sh
+++ startpxe.sh
@@ -1,3 +1,4 @@
+### Run a PXE container
 su - root
 vgcreate vgDATA /dev/vdb
 lvcreate -n lvLIB -L 5G /dev/vdb
@@ -14,3 +15,9 @@ firewall-cmd --reload
 
 buildah bud --security-opt label=disable --tag localhost/kevydotvinu/pxe:v1 .
 podman run --rm -it --privileged --net host -v "$(pwd)/tftpboot:/var/lib/tftpboot" -v "$(pwd)/dnsmasq.conf.dhcpproxy:/etc/dnsmasq.conf" --security-opt label=disable --name=pxe localhost/kevydotvinu/pxe:v1 --interface=vboxnet0
+
+### iPXE commands
+Ctrl+b
+set net0/filename pxelinux.0
+set net0/next-server X.X.X.X
+autoboot
