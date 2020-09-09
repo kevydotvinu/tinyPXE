6cd84e87ab13967229b37adfff8d6ac2e5736b08
Vinu K <kv_vinu@hotmail.com>
2020-09-09 12:32:45 +0530

    edited startpxe.sh

--- startpxe.sh
+++ startpxe.sh
@@ -21,3 +21,35 @@ Ctrl+b
 set net0/filename pxelinux.0
 set net0/next-server X.X.X.X
 autoboot
+
+### Build undionly.kpxe with script embedded
+yum install -y xz-devel.x86_64
+git clone git://git.ipxe.org/ipxe.git
+cd ipxe/src
+cat << EOF > boot.ipxe
+#!ipxe
+
+menu install menu
+item ubuntu Ubuntu install
+item centos7 Centos7 install
+item --gap
+item back install menu
+choose --timeout 20000 --default back target && goto ${target} || goto menu
+
+:ubuntu
+set ubuntu http://archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64
+initrd ${ubuntu}/initrd.gz
+kernel ${ubuntu}/linux tasks=standard vga=788 -- quiet
+boot
+
+:centos7
+set centos7 http://mirror.centos.org/centos/7/os/x86_64
+initrd ${centos7}/image/pxeboot/initrd.img
+kernel ${centos7}/image/pxeboot/vmlinuz vga=788 repo=${centos7}
+boot
+
+:back
+exit
+EOF
+make bin/undionly.kpxe EMBED=boot.ipxe
+cp bin/undionly.kpxe tftpboot/
